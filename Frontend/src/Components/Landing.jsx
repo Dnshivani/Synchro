3881654a3548630fbbe4d1c577b3b41a5bbc21bf
Shivani <missdnshivani@gmail.com>
2026-01-10 10:31:56 +0530

    frontend

---
+++ Frontend/src/Components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <div>
+            <h1 className='text-balance'>Landing Page</h1>
+            <nav className='group gap-10'>
+                <a href="#home" className='gap-2'>Home</a>
+                <a href="#about" className='gap-2'>About</a>
+                <a href="#contact">Contact</a>
+
+            </nav>
+        </div>
+    )
+}
