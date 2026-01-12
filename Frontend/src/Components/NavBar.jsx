d949c6e728cf34597d4ba2188fb74a88c306319c
Shivani <missdnshivani@gmail.com>
2026-01-12 09:11:42 +0530

    making form page

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 import ThemeToggle from './ThemeToggle'
 import Logo from './Logo'
 import Signup from './Signup'
-import { Navigate, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
-    const getStarted = () => {
 
-
-    }
     return (
+
         <div >
             <div className="navbar bg-base-100 shadow-sm bg-custom-secondary">
                 <div className="navbar-start">
+
                     <Logo />
                 </div>
                 <div className="navbar-end ">
-                    <a className="btn  text-custom-primary" onClick={getStarted} >Get Started</a>
+                    <Link to="/signup">
+                        <a className="btn  text-custom-primary" >Get Started</a>
+                    </Link>
                     <ThemeToggle />
                 </div>
             </div>
