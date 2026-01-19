4ef168421dce93513443cde2486cfef80677d3f8
harshavardhan-macha <harshavardhanmacha19@gmail.com>
2026-01-19 20:03:28 +0530

    solved the import error

--- BACKEND/routes/landingRoutes.js
+++ BACKEND/routes/landingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
 const router = express.Router();
-import { landingRecords } from "../controllers/landingControllers";
+import { landingRecords } from "../controllers/landingControllers.js";
 router.get('/', landingRecords);
 export default router;
