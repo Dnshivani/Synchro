fd109e74b1a7fa9f3a1bf1ce7f7dad4316f5e1c8
Shivani <missdnshivani@gmail.com>
2026-03-03 09:17:04 +0530

    dashboard navitems implementations

--- Frontend/src/Components/DashBoard/DashBoard.jsx
+++ Frontend/src/Components/DashBoard/DashBoard.jsx
@@ -1,12 +1,21 @@
 import Navbar from 'daisyui/components/navbar'
 import React from 'react'
 import NavBar from '../NavBar.jsx'
+import { Outlet } from 'react-router-dom';
 const DashBoard = () => {
+    const element = [{ "id": 1, "e": "Home" },
+    { "id": 2, "e": "DiscoverWorkspace" },
+    { "id": 3, "e": "Notifications" },
+    { "id": 4, "e": "Profile" },];
+
     return (
+
         <div>
-            <p>DashBOard</p>
-            <NavBar />
-            //home discover_workspace notifications profile
+            <p>DashBard</p>
+
+            <NavBar element={element} basepath='/dashboard' />
+            <Outlet />
+        // home discover_workspace notifications profile
 
 
         </div>
--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -4,16 +4,21 @@ import Logo from './Logo'
 import Signup from './Signup'
 import { Link } from 'react-router-dom'
 
-export default function NavBar() {
+export default function NavBar({ element = [], basepath = "" }) {//element=element same 
+    const traverse = () => {
+        return (
+            (element.map(ele => <Link to={`${basepath}/${ele.e.toLowerCase()}`}><p key={ele.id} > {ele.e}</p ></Link>))
 
+        )
+    }
     return (
-
         <div >
             <div className="navbar bg-base-100 shadow-sm bg-custom-secondary">
                 <div className="navbar-start">
 
                     <Logo />
                 </div>
+                <div className='flex gap-4 text-custom-primary'>{traverse()}</div>
                 <div className="navbar-end ">
                     <Link to="/signup">
                         <p className="btn btn-outline  text-custom-primary" >Get Started</p>
